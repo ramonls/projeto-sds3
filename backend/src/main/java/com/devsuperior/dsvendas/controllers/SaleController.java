eb8605f8a93f4a4f3e1e5df302d5b555858c5fe0
Ramon Lima <ramon.lima@outlook.com>
2021-06-20 17:45:30 -0300

    update

--- backend/src/main/java/com/devsuperior/dsvendas/controllers/SaleController.java
+++ backend/src/main/java/com/devsuperior/dsvendas/controllers/SaleController.java
@@ -30,12 +30,14 @@ public class SaleController {
 		return ResponseEntity.ok(list);
 	}	
 	
+	@CrossOrigin(origins = "https://ramon-dsvendas.netlify.app")
 	@GetMapping(value = "/amount-by-seller")
 	public ResponseEntity<List<SaleSumDTO>> amountGroupedBySeller() {
 		List<SaleSumDTO> list = service.amountGroupedBySeller();
 		return ResponseEntity.ok(list);
 	}	
 	
+	@CrossOrigin(origins = "https://ramon-dsvendas.netlify.app")
 	@GetMapping(value = "/success-by-seller")
 	public ResponseEntity<List<SaleSuccessDTO>> successGroupedBySeller() {
 		List<SaleSuccessDTO> list = service.successGroupedBySeller();
