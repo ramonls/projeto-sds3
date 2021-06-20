802a1366416d5ba7d4810f2883ed1a8bb3ece4be
Ramon Lima <ramon.lima@outlook.com>
2021-06-20 18:00:32 -0300

    teste000001

--- backend/src/main/java/com/devsuperior/dsvendas/controllers/SellerController.java
+++ backend/src/main/java/com/devsuperior/dsvendas/controllers/SellerController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,6 +19,7 @@ public class SellerController {
 	@Autowired
 	private SellerService service;
 	
+	@CrossOrigin(origins = "https://ramon-dsvendas.netlify.app/dashboard")
 	@GetMapping
 	public ResponseEntity<List<SellerDTO>> findAll() {
 		List<SellerDTO> list = service.findAll();
